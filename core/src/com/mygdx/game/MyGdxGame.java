35c52a52921c54280f63b500e06d777f203c085a
Yuqing Lin <linyuqing97@gmail.com>
2019-06-11 17:43:06 -0400

    play around with libGdx touch handelers

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,40 +7,66 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
+
+import java.awt.Shape;
 
 public class MyGdxGame extends Game {
 
 
 	public SpriteBatch batch;
 	private BitmapFont font;
+	private OrthographicCamera camera;
+	private ShapeRenderer sr;
+	private Vector3 pos;
+
+	Sprite sprite;
+	Sprite img;
+	float w,h,tw,th = 0;
+
 
 	@Override
 	public void create () {
+		sr = new ShapeRenderer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 
-		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.BLUE);
-		setScreen(new PlayScreen(this));
 
+		pos = new Vector3(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2,0);
 
 
 	}
 
 	@Override
 	public void render () {
+		camera.update();
+
+
+		if(Gdx.input.isTouched()){
+			pos.set(Gdx.input.getX(),Gdx.input.getY(),0);
+			camera.unproject(pos);
+
+
+		}
+
+
+
 		Gdx.gl.glClearColor(1,1,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-		font.draw(batch,"Hello world",100,400);
-		batch.end();
+		sr.begin(ShapeRenderer.ShapeType.Filled);
+		sr.setColor(Color.BLUE);
+		sr.circle(pos.x,pos.y,64);
+		sr.end();
 
 	}
 	
 	@Override
 	public void dispose () {
-	batch.dispose();
-	font.dispose();
+	sr.dispose();
 	}
 }
