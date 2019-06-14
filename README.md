72f53fe45244eef9cd2615dbee6f9fea8cb73eef
Yuqing Lin <linyuqing97@gmail.com>
2019-06-13 23:41:47 -0400

    equation form seted

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # CS441-Project-2-Android
 
+This is the CS441 Second project created base on libGDX.
+
+Objective:
+
+This project is base on android studio with a game API called libGDX. libGDX provides functionalities for responding users' gestrues with different animations such as tap, pan , rotation or changeing seting on an object.
+
+In this project, I have implemented an equation called the bearcat magic function. What happens if a bear and a cat get together?  By implement this equation, I have created five sprites which is libGdx API allows me to display and play around the images/object on the screen. Then I have implemented an input processor with a gesture detector along to handle users' finger gestures on the screen. This interprocessor and gesture detector provides the functionalities to handle events such as moving the object around the screen, taping the screen with different responses from the program or rotation, zome the object. 
+With the capabilities of moving object around the screen in mind, an idea pops up on my mind, what happens if two images combined? So, I implemented my Bearcat magic function by putting a "bear + cat = ? "form on the screen when users first launch the program. By setting up a gesture detector on the cat image, image cat now is an object that user can play around with.  The cat image will follow users finger movement on the screen. The cat will also pop up at users taping position on the screen or returns to it's inital position if double taping. Besides, users can rotate the cat image or long press the screen to change the brightness of the cat image. Most importantly, if users move the cat on top of the bear, the equation  result will pop up on the screen which is BEARCAT!!!(Sounds stupid , but I like bing and bearcat!)
+This bearcat image is trigerd by collision handeling  using gdx rectangle API. This api tranfer two sprites into two rectangles. With rectangle overlaping check, if these two images are overlaped,  new sprite with bearcat image get loaded in and displays on top of all other sprites. Users can bring back the orignial function by double tapping the screen.  
 
 
 
-Time lin:
-6/8 Created Github repository
-6/9 Created the project with libGdx
-6/11 Play around the libGdx touch handeler, create a blue doc which follow user's gestrue(No multitouch)
+
+Time line:
+6/8 Created Github repository.
+6/9 Created the project with libGdx.
+6/11 Play around the libGdx touch handeler, create a blue doc which follow user's gestrue(No multitouch).
+6/12 Added images on libgdx sprite, applied different animation on sprite including tap, pan, rotation.
+6/13 Finalize the bearcat equation, set all images to it's corrent position. Implement collision handeler.Seted the bear and cat image into rectangles handelers. If the bear and cat image overlap to each others, a new bearcat image would pop up. 
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -16,14 +16,25 @@ import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-
-import java.awt.Shape;
+import com.badlogic.gdx.math.Rectangle;
+//
+//import java.awt.Rectangle;
+//import java.awt.Shape;
 
 public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener {
 
 
 	public SpriteBatch batch;
 	public Sprite sprite;
+	public Sprite sprite2;
+	public Sprite sprite3;
+    public Sprite equalSign;
+    public Sprite plusSign;
+    public Sprite questionSign;
+
+
+	public Rectangle rectangleBearcat;
+	public Rectangle rectangleBadlogic;
 	private Texture img;
 	OrthographicCamera camera;
 	GestureDetector gestureDetector;
@@ -34,9 +45,35 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 	public void create () {
 		batch = new SpriteBatch();
 
-		sprite = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
-		sprite.setPosition(-sprite.getWidth()/2,-sprite.getHeight()/2);
-		sprite.setCenter(0.5f,0.5f);
+
+		sprite = new Sprite(new Texture(Gdx.files.internal("cat.jpg")));
+		sprite2 = new Sprite(new Texture(Gdx.files.internal("bear.jpg")));
+		sprite3= new Sprite(new Texture(Gdx.files.internal("bearcat2.png")));
+        equalSign = new Sprite(new Texture(Gdx.files.internal("equal.jpg")));
+        plusSign = new Sprite(new Texture(Gdx.files.internal("plus.jpg")));
+        questionSign = new Sprite(new Texture(Gdx.files.internal("question.jpg")));
+
+
+
+
+		sprite.setSize(256,256);
+		sprite2.setSize(256,256);
+		questionSign.setSize(128,256);
+		equalSign.setSize(256,256);
+		plusSign.setSize(256,256);
+
+		sprite.setPosition(-600,0);
+		plusSign.setPosition(-375,0);
+		sprite2.setPosition(-100,0);
+		equalSign.setPosition(150,0);
+		questionSign.setPosition(350,0);
+
+
+		rectangleBearcat = new Rectangle(sprite.getX(),sprite.getY(),sprite.getWidth(),sprite.getHeight());
+		rectangleBadlogic = new Rectangle(sprite2.getX(),sprite2.getY(),sprite2.getWidth(),sprite2.getHeight());
+
+
+		//sprite.setCenter(0.5f,0.5f);
 		camera = new OrthographicCamera(1280,720);
 		camera.update();
 		gestureDetector = new GestureDetector(this);
@@ -53,8 +90,26 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 		Gdx.gl.glClearColor(1,1,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
+		rectangleBearcat = sprite.getBoundingRectangle();
+		rectangleBadlogic = sprite2.getBoundingRectangle();
+
+		boolean isOverlaping = rectangleBadlogic.overlaps(rectangleBearcat);
+
 		batch.begin();
 		sprite.draw(batch);
+		plusSign.draw(batch);
+		equalSign.draw(batch);
+		questionSign.draw(batch);
+		sprite2.draw(batch);
+		if(isOverlaping){
+			System.out.println("noOverlap");
+			sprite3.setPosition(-500,-550);
+			sprite3.setSize(1024,1024);
+			sprite3.draw(batch);
+//	        sprite.setPosition(-sprite.getWidth()/2,-sprite.getHeight()/2);
+//			sprite2.setPosition(-sprite.getWidth(),-sprite.getHeight());
+		}
+
 		batch.end();
 	}
 
@@ -108,10 +163,12 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 
 	@Override
 	public boolean pan(float x, float y, float deltaX, float deltaY) {
+
 		Vector3 touchPos = new Vector3(x,y,0);
 		camera.unproject(touchPos);
 
 		sprite.setPosition(touchPos.x-sprite.getWidth()/2,touchPos.y-sprite.getHeight()/2);
+
 
 
 		return true;
@@ -124,7 +181,7 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 
 	@Override
 	public boolean zoom(float initialDistance, float distance) {
-		sprite.setSize(distance,distance);
+		//sprite.setSize(distance,distance);
 
 		return true;
 	}
